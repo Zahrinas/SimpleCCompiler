b5a600b1fe42eedaacebecad2a213916b6a532a6
ausar47 <hjjausar@outlook.com>
2023-05-19 00:05:22 +0800

    生成AST大框架，应该是这样写的吧

--- SimpleCCompiler/AST.h
+++ SimpleCCompiler/AST.h
@@ -4,25 +4,25 @@
 #include "test.h"
 
 enum class AST_type {
-	seq_tree, eseq_tree,
-	decl_inst, return_inst,
-	call_inst,
-	int_type,
-	name, string, constant,
-	plus, minus, multiply, divide,
+    seq_tree, eseq_tree,
+    decl_inst, return_inst,
+    call_inst,
+    int_type,
+    name, string, constant,
+    plus, minus, multiply, divide, and, or , not, mod, assign, equal, not_equal, greater, less, greater_equal, less_equal, band, bor, bxor,
 
 
-	func_decl, label_decl,
-	void_type, float_type,
-	if_inst, for_inst, while_inst,
-	goto_inst
+    func_decl, label_decl,
+    void_type, double_type,
+    if_inst, for_inst, while_inst,
+    goto_inst
 };
 
 struct AST_node {
-	AST_type type;
-	void* ptr;
+    AST_type type;
+    void *ptr;
 
-	AST_node(AST_type t, void* p = nullptr);
+    AST_node(AST_type t, void *p = nullptr);
 };
 
 typedef tree<AST_node> AST;
